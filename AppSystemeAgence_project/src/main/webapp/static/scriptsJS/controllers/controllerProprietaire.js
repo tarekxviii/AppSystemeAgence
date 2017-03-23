6973a935b7b4f90805c3eca29c6f98be5cc54a83
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2017-03-23 15:34:26 +0100

    finition

--- AppSystemeAgence_project/src/main/webapp/static/scriptsJS/controllers/controllerProprietaire.js
+++ AppSystemeAgence_project/src/main/webapp/static/scriptsJS/controllers/controllerProprietaire.js
@@ -3,68 +3,96 @@
  */
 
 app.controller("addPropCtrl",
-		function($scope, $rootScope, proprietaireProvider, $location) {
-			$scope.proprietaireForm = {
-				nom_prop : "",
-				adresse_prop : "",
-				telephonePrive_prop : "",
-				telephoneTravail_prop : "",
-				password_prop : "",
-			};
+	function($scope, $rootScope, proprietaireProvider, $location) {
+		$scope.proprietaireForm = {
+			nom_prop : "",
+			adresse_prop : "",
+			telephonePrive_prop : "",
+			telephoneTravail_prop : "",
+			password_prop : "",
+		};
 
-			$scope.ajouterProp = function() {
-				console.log("-----------    "+$scope.proprietaireForm);
-				proprietaireProvider.addProprietaire($scope.proprietaireForm, function(callback) {
+		$scope.ajouterProp = function() {
+			console.log("-----------    " + $scope.proprietaireForm);
+			proprietaireProvider.addProprietaire($scope.proprietaireForm, function(callback) {
 
-					if (callback != undefined && callback != "") {
-						$location.path("accueil");
-					}
-				})
-			}
-		}).controller("getPropCtrl",
-		function($scope, $rootScope, proprietaireProvider, $location) {
-			proprietaireProvider.getProprietaire(function(callback) {
-				$scope.proprietaires = callback.data;
-			});
-			// Méthode pour supprimer un propriétaire à partir du lien de la table
-			$scope.deleteLien = function(id) {
+				if (callback != undefined && callback != "") {
+					$location.path("accueil");
+				}
+			})
+		}
+	}).controller("getPropCtrl",
+	function($scope, $rootScope, proprietaireProvider, $location) {
+		proprietaireProvider.getProprietaire(function(callback) {
+			$scope.proprietaires = callback.data;
+		});
+		// Méthode pour supprimer un propriétaire à partir du lien de la table
+		$scope.deleteLien = function(id) {
 
-				proprietaireProvider.deleteProprietaire(id, function(callback) {
+			proprietaireProvider.deleteProprietaire(id, function(callback) {
 
-					if (callback != undefined && callback != "") {
-						proprietaireProvider.getProprietaire(function(callback) {
-							$scope.proprietaires = callback.data;
+				if (callback != undefined && callback != "") {
+					proprietaireProvider.getProprietaire(function(callback) {
+						$scope.proprietaires = callback.data;
 
-						});
-					}
-				})
-			}
-			// Déclarer le pays dans le rootScope
-			$rootScope.proprietaireModif = {
-				id_prop : undefined,
-				nom_prop : "",
-				adresse_prop : "",
-				telephonePrive_prop : "",
-				telephoneTravail_prop : "",
-				password_prop : "",
-			}
-			$scope.updateLien = function(prop) {
-				$rootScope.proprietaireModif.id_prop = prop.id_prop;
-				$rootScope.proprietaireModif.nom_prop = prop.nom_prop;
-				$rootScope.proprietaireModif.adresse_prop = proprietaireModif.adresse_prop;
-				$rootScope.proprietaireModif.telephonePrive_prop = prop.telephonePrive_prop;
-				$rootScope.proprietaireModif.telephoneTravail_prop = prop.telephoneTravail_prop;
-				$location.path("modifier");
-			}
+					});
+				}
+			})
+		}
+		// Déclarer le pays dans le rootScope
+		$rootScope.proprietaireModif = {
+			id_prop : undefined,
+			nom_prop : "",
+			adresse_prop : "",
+			telephonePrive_prop : "",
+			telephoneTravail_prop : "",
+			password_prop : "",
+		}
+		$scope.updateLien = function(prop) {
+			$rootScope.proprietaireModif.id_prop = prop.id_prop;
+			$rootScope.proprietaireModif.nom_prop = prop.nom_prop;
+			$rootScope.proprietaireModif.adresse_prop = prop.adresse_prop;
+			$rootScope.proprietaireModif.telephonePrive_prop = prop.telephonePrive_prop;
+			$rootScope.proprietaireModif.telephoneTravail_prop = prop.telephoneTravail_prop;
+			$location.path("modifier");
+		}
 
-		}).controller("deletePropCtrl", function($scope, proprietaireProvider, $location) {
-			$scope.id = undefined;
-			$scope.supprimerProp = function() {
-				proprietaireProvider.deleteProprietaire($scope.id, function(callback) {
+	}).controller("deletePropCtrl", function($scope, proprietaireProvider, $location) {
+	$scope.id = undefined;
+	$scope.supprimerProp = function() {
+		proprietaireProvider.deleteProprietaire($scope.id, function(callback) {
 
-					if (callback != undefined && callback != "") {
-						$location.path("accueil");
-					}
-				})
+			if (callback != undefined && callback != "") {
+				$location.path("accueil");
 			}
 		})
+	}
+}).controller("updatePropCtrl",
+	function($scope, $rootScope, proprietaireProvider, $location) {
+		if ($rootScope.proprietaireModif.id_prop ==undefined)  {
+			$scope.proprietaireModif={
+			id_prop : undefined,
+			nom_prop : "",
+			adresse_prop : "",
+			telephonePrive_prop : "",
+			telephoneTravail_prop : "",
+			password_prop : "",
+			};
+		}else{
+			$scope.proprietaireModif.id_prop = $rootScope.prop.id_prop;
+			$scope.proprietaireModif.nom_prop = $rootScope.prop.nom_prop;
+			$scope.proprietaireModif.adresse_prop = $rootScope.prop.adresse_prop;
+			$scope.proprietaireModif.telephonePrive_prop = $rootScope.prop.telephonePrive_prop;
+			$scope.proprietaireModif.telephoneTravail_prop = $rootScope.prop.telephoneTravail_prop;
+			
+		}
+
+		$scope.modifierProp = function() {
+			proprietaireProvider.updateProprietaire($scope.proprietaireModif, function(callback) {
+
+				if (callback != undefined && callback != "") {
+					$location.path("accueil");
+				}
+			})
+		}
+	})
--- AppSystemeAgence_project/src/main/webapp/static/scriptsJS/providers/providerProprietaire.js
+++ AppSystemeAgence_project/src/main/webapp/static/scriptsJS/providers/providerProprietaire.js
@@ -19,21 +19,21 @@ app.factory("proprietaireProvider", function($http) {
 		}).error(function(response) {
 			console.log('Erreur : ' + response.statusText);
 
-		})
+		});
 
 	}
 	function getProprietaire(callback) {
 		$http({
 			method : 'GET',
 			url : urlglobal + '/proprietaire/getAll',
-		}).success(function(response) {
+		}).then(function successCallback(response) {
 			console.log(response.data);
 			callback(response);
 
-		}).error(function(response) {
+		},function errorCallback(response) {
 			console.log('Erreur : ' + response.statusText);
 
-		})
+		});
 
 	}
 	function deleteProprietaire(id,callback) {
@@ -47,12 +47,31 @@ app.factory("proprietaireProvider", function($http) {
 		}).error(function(response) {
 			console.log('Erreur : ' + response.statusText);
 
-		})
+		});
+
+	}
+	function updateProprietaire(proprietaireModif, callback) {
+		$http({
+			method : 'PUT',
+			url : urlglobal + '/proprietaire/update',
+			data : angular.toJson(proprietaireModif),
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).success(function(response) {
+			console.log(response);
+			callback(response);
+
+		}).error(function(response) {
+			console.log('Erreur : ' + response.statusText);
+
+		});
 
 	}
 	return {
 		addProprietaire : addProprietaire,
 		getProprietaire : getProprietaire,
 		deleteProprietaire : deleteProprietaire,
+		updateProprietaire : updateProprietaire,
 	}
 })
