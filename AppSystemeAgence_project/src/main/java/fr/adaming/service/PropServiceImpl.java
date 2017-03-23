bea599f032ac607cadabe07a801d84c2de8ade44
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-23 11:49:12 +0100

    Annotations

--- AppSystemeAgence_project/src/main/java/fr/adaming/service/PropServiceImpl.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/PropServiceImpl.java
@@ -1,9 +1,17 @@
 package fr.adaming.service;
 
 import fr.adaming.entities.Proprietaire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class PropServiceImpl implements IPropService {
 
+	@Autowired
+	IPropDao propdao;
+	
 	@Override
 	public void addService(Proprietaire prop) {
 		// TODO Auto-generated method stub
