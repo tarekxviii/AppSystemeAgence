5473d5a9250022995ca9286211fbd9512d4ec3e4
inti0226 <inti0226@INTI-0226.home>
2017-03-23 12:18:59 +0100

    Definition des signatures des méthodes

--- AppSystemeAgence_project/src/main/java/fr/adaming/dao/IPropDao.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/dao/IPropDao.java
@@ -1,9 +1,20 @@
 package fr.adaming.dao;
 
+import java.util.List;
+
 import fr.adaming.entities.Proprietaire;
 
 public interface IPropDao{
 
 	public void addDao(Proprietaire prop);
+	
+	public void deleteDao(int id);
+	
+	public void updateDao(Proprietaire prop);
+	
+	public List<Proprietaire> getAllProprietaire();
+	
+	public Proprietaire getProprietaireById(int id);
+	
 	
 }
