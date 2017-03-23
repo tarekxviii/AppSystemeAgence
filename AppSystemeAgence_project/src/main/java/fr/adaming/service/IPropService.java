d4a7cccfdeffd7dcf4dca667ec09d517b86f5216
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-23 11:45:05 +0100

    PropService

---
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/IPropService.java
@@ -0,0 +1,9 @@
+package fr.adaming.service;
+
+import fr.adaming.entities.Proprietaire;
+
+public interface IPropService {
+	
+	public void addService(Proprietaire prop);
+
+}
---
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/PropServiceImpl.java
@@ -0,0 +1,15 @@
+package fr.adaming.service;
+
+import fr.adaming.entities.Proprietaire;
+
+public class PropServiceImpl implements IPropService {
+
+	@Override
+	public void addService(Proprietaire prop) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+
+}
