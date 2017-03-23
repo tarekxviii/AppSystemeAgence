d94368e6c27fb7099fc9ea150b8cdd716d4dd0db
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-23 12:26:59 +0100

    Mise en place du service pour Prop

--- AppSystemeAgence_project/src/main/java/fr/adaming/service/IPropService.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/IPropService.java
@@ -1,9 +1,19 @@
 package fr.adaming.service;
 
+import java.util.List;
+
 import fr.adaming.entities.Proprietaire;
 
 public interface IPropService {
 	
 	public void addService(Proprietaire prop);
+	
+	public void deleteService(int id);
+	
+	public void updateService(Proprietaire prop);
+	
+	public List<Proprietaire> getAllProprietaire();
+	
+	public Proprietaire getProprietaireById(int id);
 
 }
--- AppSystemeAgence_project/src/main/java/fr/adaming/service/PropServiceImpl.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/PropServiceImpl.java
@@ -2,6 +2,9 @@ package fr.adaming.service;
 
 import fr.adaming.dao.IPropDao;
 import fr.adaming.entities.Proprietaire;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +20,30 @@ public class PropServiceImpl implements IPropService {
 	public void addService(Proprietaire prop) {
 		propdao.addDao(prop);
 		
+	}
+
+	@Override
+	public void deleteService(int id) {
+		propdao.deleteDao(id);
+		
+	}
+
+	@Override
+	public void updateService(Proprietaire prop) {
+	propdao.updateDao(prop);
+		
+	}
+
+	@Override
+	public List<Proprietaire> getAllProprietaire() {
+		
+		return propdao.getAllProprietaire();
+	}
+
+	@Override
+	public Proprietaire getProprietaireById(int id) {
+		
+		return propdao.getProprietaireById(id);
 	}
 	
 
