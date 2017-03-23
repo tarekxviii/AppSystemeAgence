5664180cc29d839fab0519c469ad5b7e4ce9b905
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-23 15:02:35 +0100

    Responsable

--- AppSystemeAgence_project/src/main/java/fr/adaming/dao/RespDaoImpl.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/dao/RespDaoImpl.java
@@ -7,17 +7,18 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import fr.adaming.entities.Proprietaire;
+
 import fr.adaming.entities.Responsable;
 
 @Repository
 public class RespDaoImpl implements IRespDao {
-	
+
 	@Autowired
 	private SessionFactory sf;
 
 	/**
-	 * @param sf the sf to set
+	 * @param sf
+	 *            the sf to set
 	 */
 	public void setSf(SessionFactory sf) {
 		this.sf = sf;
@@ -27,31 +28,29 @@ public class RespDaoImpl implements IRespDao {
 	public void addDao(Responsable resp) {
 
 		// Creation de la session
-		Session s=sf.getCurrentSession();
-		
-		s.save(resp);	
-		
+		Session s = sf.getCurrentSession();
+
+		s.save(resp);
+
 	}
 
 	@Override
 	public void deleteDao(int id) {
 
+		Session s = sf.getCurrentSession();
 
-		Session s=sf.getCurrentSession();
-		
 		s.delete(s.get(Responsable.class, id));
-		
+
 	}
 
 	@Override
 	public void updateDao(Responsable resp) {
 
-		Session s=sf.getCurrentSession();
-		
+		Session s = sf.getCurrentSession();
+
 		// Recupération du propriétaire a modifier
-		Responsable r1=(Responsable) s.get(Responsable.class, resp.getId_resp());
-		
-		
+		Responsable r1 = (Responsable) s.get(Responsable.class, resp.getId_resp());
+
 		// Affectation des nouvelles valeurs aux attributs
 		r1.setId_resp(resp.getId_resp());
 		r1.setMail_resp(resp.getMail_resp());
@@ -59,29 +58,28 @@ public class RespDaoImpl implements IRespDao {
 		r1.setrListeContrat(resp.getrListeContrat());
 		r1.setrListeVisite(resp.getrListeVisite());
 
-
 		// Modification dans la base de données
 		s.saveOrUpdate(r1);
-		
+
 	}
 
 	@Override
 	public List<Responsable> getAllResponsable() {
-Session s=sf.getCurrentSession();
-		
+		Session s = sf.getCurrentSession();
+
 		// Ecriture de la requete
-		String req="select r from Responsable r";
-		
+		String req = "select r from Responsable r";
+
 		return s.createQuery(req).list();
 	}
 
 	@Override
 	public Responsable getResponsableById(int id) {
 
-		Session s=sf.getCurrentSession();
-		
+		Session s = sf.getCurrentSession();
+
 		return (Responsable) s.get(Responsable.class, id);
-		
+
 	}
 
 }
--- AppSystemeAgence_project/src/main/java/fr/adaming/service/IRespService.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/IRespService.java
@@ -1,5 +1,37 @@
 package fr.adaming.service;
 
+import java.util.List;
+
+import fr.adaming.entities.Responsable;
+
 public interface IRespService {
+	
+	/**
+	 * @param resp
+	 */
+	public void addService(Responsable resp);
+	
+	/**
+	 * @param id
+	 */
+	public void deleteService(int id);
+	
+	/**
+	 * @param resp
+	 */
+	public void updateService(Responsable resp);
+	
+	/**
+	 * @return
+	 */
+	public List<Responsable> getAllResponsable();
+	
+	/**
+	 * @param id
+	 * @return
+	 */
+	public Responsable getResponsableById(int id);
+	
+	
 
 }
