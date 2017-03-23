d4c68f6e80e42e936122d55513c4f0a9f447fb1b
inti0226 <inti0226@INTI-0226.home>
2017-03-23 11:11:39 +0100

    Dao

---
+++ AppSystemeAgence_project/src/main/java/fr/adaming/dao/PropDaoImpl.java
@@ -0,0 +1,33 @@
+package fr.adaming.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.adaming.entities.Proprietaire;
+
+public class PropDaoImpl implements IPropDao {
+
+	
+	@Autowired
+	private SessionFactory sf;
+
+	/**
+	 * @param sf the sf to set
+	 */
+	public void setSf(SessionFactory sf) {
+		this.sf = sf;
+	}
+	
+	
+	@Override
+	public void addDao(Proprietaire prop) {
+		
+		// Creation de la session
+		Session s=sf.getCurrentSession();
+		
+		s.save(prop);	
+		
+	}
+
+}
