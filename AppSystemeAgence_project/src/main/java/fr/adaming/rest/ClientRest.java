3b5f631462546e4fd80af9dedca4de2de83db1d0
inti0301 <inti0301@DESKTOP-3HO9GU6.home>
2017-03-24 14:02:56 +0100

    rzsfthtrdhtrhrtd

--- AppSystemeAgence_project/src/main/java/fr/adaming/rest/ClientRest.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/ClientRest.java
@@ -1,5 +1,6 @@
 package fr.adaming.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.adaming.entities.Categorie;
 import fr.adaming.entities.Client;
+import fr.adaming.service.IBienService;
 import fr.adaming.service.IClientService;
 
 @RestController
@@ -24,6 +27,22 @@ public class ClientRest {
 		this.clientService = clientService;
 	}
 	
+	@Autowired
+	IBienService bienService;
+	
+	/**
+	 * @param bienService the bienService to set
+	 */
+	public void setBienService(IBienService bienService) {
+		this.bienService = bienService;
+	}
+
+
+	/**
+	 * 
+	 * @param clientAdd
+	 * @return
+	 */
 	@RequestMapping(value="/add",method=RequestMethod.POST, consumes="application/json", produces="application/json")
 	public int addClientWS(@RequestBody Client clientAdd){
 		try{
@@ -36,6 +55,11 @@ public class ClientRest {
 	}
 	
 
+	/**
+	 * 
+	 * @param clientUpdate
+	 * @return
+	 */
 	@RequestMapping(value="/update", method=RequestMethod.PUT, consumes="application/json", produces="application/json")
 	public int updateClientWS(@RequestBody Client clientUpdate){
 		try{
@@ -47,7 +71,11 @@ public class ClientRest {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param id_client
+	 * @return
+	 */
 	@RequestMapping(value="/delete/{id_param}", method=RequestMethod.DELETE, produces="application/json")
 	public int deleteClientWS(@PathVariable("id_param") int id_client){
 		try{
@@ -58,15 +86,47 @@ public class ClientRest {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value="/getAll",method=RequestMethod.GET, produces="application/json")
 	public List<Client> getAllClientWS(){
 		return clientService.getAllClient();
 	}
 	
+	/**
+	 * 
+	 * @param id_client
+	 * @return
+	 */
 	@RequestMapping(value="/getById",method=RequestMethod.GET, produces="application/json")
 	public Client getClientById(@PathVariable("id_param") int id_client){
 			return clientService.getClientById(id_client); 
 
+	}
+	
+	
+	@RequestMapping(value="/updateCatClient", method=RequestMethod.PUT, consumes="application/json", produces="application/json")
+	public int updateCatClientWS(@RequestBody Client clientUpdate, @RequestBody Categorie catUpdate){
+		try{
+			Client clientChanger;
+			Categorie catChanger;
+			List<Categorie> listeInt = new ArrayList<>();
+			
+			catChanger = bienService.getCategoriByName(catUpdate.getTypeBien());
+			clientChanger = clientService.getClientById(clientUpdate.getId_client());
+			
+			listeInt.add(catChanger);
+			
+			clientChanger.setcListeInteret(listeInt);
+			
+			clientService.updateService(clientUpdate);
+			
+			return new Integer (1);
+		}catch (Exception e) {
+			return new Integer (0);
+		}
 	}
 	
 	
