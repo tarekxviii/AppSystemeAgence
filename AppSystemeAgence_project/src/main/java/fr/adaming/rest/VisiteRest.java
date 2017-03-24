3425bb045ec6b85f64f3a4c5a887de7faf35b263
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-24 15:09:28 +0100

    responsable

--- AppSystemeAgence_project/src/main/java/fr/adaming/rest/VisiteRest.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/VisiteRest.java
@@ -1,11 +1,42 @@
 package fr.adaming.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.service.IBienService;
+import fr.adaming.service.IClientService;
+import fr.adaming.service.IRespService;
+import fr.adaming.service.IVisiteService;
 
 @RestController
 @RequestMapping(value = "/visite")
 public class VisiteRest {
+
+	@Autowired
+	private IClientService clientService;
+	@Autowired
+	private IBienService bienService;
+	@Autowired
+	private IRespService respService;
+	@Autowired
+	private IVisiteService visiteService;
+
+	public void setClientService(IClientService clientService) {
+		this.clientService = clientService;
+	}
+
+	public void setBienService(IBienService bienService) {
+		this.bienService = bienService;
+	}
+
+	public void setRespService(IRespService respService) {
+		this.respService = respService;
+	}
+
+	public void setVisiteService(IVisiteService visiteService) {
+		this.visiteService = visiteService;
+	}
 	
 	
 
