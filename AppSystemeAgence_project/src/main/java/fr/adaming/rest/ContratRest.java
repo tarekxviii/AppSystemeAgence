3f59ef7455629ddea8b9f174f10a90721c8f268b
inti0226 <inti0226@INTI-0226.home>
2017-03-24 11:40:19 +0100

    restContrat

--- AppSystemeAgence_project/src/main/java/fr/adaming/rest/ContratRest.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/ContratRest.java
@@ -1,7 +1,13 @@
 package fr.adaming.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import fr.adaming.entities.Bien;
+import fr.adaming.entities.Client;
+import fr.adaming.entities.Contrat;
+import fr.adaming.entities.Proprietaire;
+import fr.adaming.entities.Responsable;
 import fr.adaming.service.IContratService;
 
 public class ContratRest {
@@ -17,7 +23,18 @@ public class ContratRest {
 	}
 	
 	
-	public 
+	public int addContratWs(@RequestBody Contrat contrat,@RequestBody Client client,@RequestBody Bien bien,@RequestBody Responsable resp){
+		
+		try{
+			
+			return new Integer(1);
+		} catch(Exception e) {
+			
+			return new Integer(0);
+			
+		}
+		
+	}
 	
 
 }
