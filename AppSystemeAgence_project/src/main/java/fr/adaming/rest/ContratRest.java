ebf5d298d0bf36dc04e85f3bc9dced619a5db8de
inti0226 <inti0226@INTI-0226.home>
2017-03-24 11:35:23 +0100

    Push

---
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/ContratRest.java
@@ -0,0 +1,23 @@
+package fr.adaming.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.adaming.service.IContratService;
+
+public class ContratRest {
+	
+	@Autowired
+	private IContratService contratService;
+
+	/**
+	 * @param contratService the contratService to set
+	 */
+	public void setContratService(IContratService contratService) {
+		this.contratService = contratService;
+	}
+	
+	
+	public 
+	
+
+}
--- AppSystemeAgence_project/src/main/java/fr/adaming/service/ContratServiceImpl.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/service/ContratServiceImpl.java
@@ -1,10 +1,13 @@
 package fr.adaming.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.dao.IContratDao;
+import fr.adaming.entities.Contrat;
 
 @Service
 @Transactional
@@ -15,6 +18,37 @@ public class ContratServiceImpl implements IContratService {
 
 	public void setContratDao(IContratDao contratDao) {
 		this.contratDao = contratDao;
+	}
+
+	@Override
+	public void addService(Contrat contrat) {
+		
+		contratDao.addDao(contrat);
+		
+	}
+
+	@Override
+	public void deleteService(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateService(Contrat contrat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Contrat> getAllContrat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contrat getContratById() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
