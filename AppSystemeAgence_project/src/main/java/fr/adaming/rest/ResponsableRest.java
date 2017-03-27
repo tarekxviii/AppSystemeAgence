7906fa4b5eb6dad10e4c83ad2886e39d2928476b
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2017-03-27 16:46:30 +0200

    contrat et responsable rest

--- AppSystemeAgence_project/src/main/java/fr/adaming/rest/ResponsableRest.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/ResponsableRest.java
@@ -48,13 +48,13 @@ public class ResponsableRest {
 	/**
 	 * Controller: méthode ajouter un responsable
 	 * @param respAdd
-	 * @return
+	 * @return int 1 ou 0 suivant la réussite ou l'échec
 	 */
 	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public int addRespWS(@RequestBody Responsable respAdd) {
 		try {
 			System.out.println("----------------------------- " + respAdd.toString());
-			responsableService.addService(respAdd);
+			responsableService.addService(respAdd); // Ajoute le responsable
 			return new Integer(1);
 		} catch (Exception e) {
 			return new Integer(0);
@@ -66,13 +66,13 @@ public class ResponsableRest {
 	/**
 	 * Controller: méthode modifier un responsable
 	 * @param respUpdate
-	 * @return
+	 * @return int 1 ou 0 suivant la réussite ou l'échec
 	 */
 	@RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
 	public int updateRespWS(@RequestBody Responsable respUpdate) {
 		try {
 			responsableService.updateService(respUpdate);
-			return new Integer(1);
+			return new Integer(1); // modifie le responsable
 		} catch (Exception e) {
 			return new Integer(0);
 		}
@@ -83,12 +83,12 @@ public class ResponsableRest {
 	/**
 	 * Controller: méthode supprimer un responsable
 	 * @param id_resp
-	 * @return
+	 * @return int 1 ou 0 suivant la réussite ou l'échec
 	 */
 	@RequestMapping(value = "/delete/{id_param}", method = RequestMethod.DELETE, produces = "application/json")
 	public int deleteRespWS(@PathVariable("id_param") int id_resp) {
 		try {
-			responsableService.deleteService(id_resp);
+			responsableService.deleteService(id_resp); // Supprime le responsable avec son id
 			return new Integer(1);
 		} catch (Exception e) {
 			return new Integer(0);
@@ -99,12 +99,12 @@ public class ResponsableRest {
 	
 	/**
 	 * Controller: méthode afficher la liste de tout les responsables
-	 * @return
+	 * @return Liste de responsables
 	 */
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET, produces = "application/json")
 	public List<Responsable> getAllRespWS() {
 
-		return responsableService.getAllResponsable();
+		return responsableService.getAllResponsable(); // Affiche la liste des responsables
 	}
 
 	///////////////////////////Methode pour afficher un responsable par son id////////////////////////
@@ -112,11 +112,11 @@ public class ResponsableRest {
 	/**
 	 * Controller: méthode afficher un responsable par son id
 	 * @param id_resp
-	 * @return
+	 * @return un responsable
 	 */
 	@RequestMapping(value = "/getById", method = RequestMethod.GET, produces = "application/json")
 	public Responsable getPropById(@PathVariable("id_param") int id_resp) {
-		return responsableService.getResponsableById(id_resp);
+		return responsableService.getResponsableById(id_resp); // Retourne le responsable avec son id
 
 	}
 	
@@ -125,12 +125,12 @@ public class ResponsableRest {
 	/**
 	 * Controller: méthode pour afficher la liste des biens d'une catégorie
 	 * @param cat
-	 * @return
+	 * @return Liste des biens
 	 */
 	@RequestMapping(value = "/getBienCat/{nom_cat}", method = RequestMethod.GET, produces = "application/json")
 	public List<Bien> getBienByCatWS(@PathVariable("nom_cat") String nom_cat){
-		Categorie cat = bienService.getCategoriByName(nom_cat);
-		return responsableService.getBienByCatService(cat);
+		Categorie cat = bienService.getCategoriByName(nom_cat);// retourne la catégorie en prenant son nom en paramètre
+		return responsableService.getBienByCatService(cat); // retourne la liste des biens de la catégorie dont le nil est en paramètre
 	}
 
 }
