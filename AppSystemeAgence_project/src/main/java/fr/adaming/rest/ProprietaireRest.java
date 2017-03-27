4df1f5094e665a44839c04bc4c50858ebc701ed6
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2017-03-27 16:38:04 +0200

    propriétaire rest

--- AppSystemeAgence_project/src/main/java/fr/adaming/rest/ProprietaireRest.java
+++ AppSystemeAgence_project/src/main/java/fr/adaming/rest/ProprietaireRest.java
@@ -15,109 +15,119 @@ import fr.adaming.entities.Proprietaire;
 import fr.adaming.service.IPropService;
 
 @RestController
-@RequestMapping(value="/proprietaire")
+@RequestMapping(value = "/proprietaire")
 public class ProprietaireRest {
-	
+
 	@Autowired
 	IPropService proprietaireService;
 
 	/**
 	 * Setter pour l'injection des méthodes de proprietaireService
 	 * 
-	 * @param proprietaireService the proprietaireService to set
+	 * @param proprietaireService
+	 *            the proprietaireService to set
 	 */
 	public void setProprietaireService(IPropService proprietaireService) {
 		this.proprietaireService = proprietaireService;
 	}
-	
-	///////////////////////////Methode ajouter un Proprietaire ////////////////////////
+
+	/////////////////////////// Methode ajouter un Proprietaire
+	/////////////////////////// ////////////////////////
 	/**
 	 * Methode ajouter un Proprietaire
 	 * 
 	 * @param prop
 	 * @return 1 si prop ajouter si non 0
 	 */
-	@RequestMapping(value="/add",method=RequestMethod.POST, consumes="application/json", produces="application/json")
-	public int addPropWS(@RequestBody Proprietaire propAdd){
-		try{
-			System.out.println("----------------------------- "+propAdd.getNom_prop());
+	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public int addPropWS(@RequestBody Proprietaire propAdd) {
+		try {
+			// Ajout d'un propriétaire en Bdd
 			proprietaireService.addService(propAdd);
-			return new Integer (1);
-		}catch(Exception e){
-			return new Integer (0);
+			// Renvoie 1 dans la console si l'opération est réalisée avec succès
+			return new Integer(1);
+		} catch (Exception e) {
+			// Renvoie 1 dans la console si l'opération n'a pas été réalisée
+			// avec succès
+			return new Integer(0);
 		}
 	}
-		
-		///////////////////////////Methode modifier un Proprietaire ////////////////////////
-	
-	
-		/**
-		 * Methode modifier un Proprietaire
-		 * 
-		 * @param propUpdate
-		 * @return
-		 */
-		@RequestMapping(value="/update", method=RequestMethod.PUT, consumes="application/json", produces="application/json")
-		public int updatePropWS(@RequestBody Proprietaire propUpdate){
-			try{
-				proprietaireService.updateService(propUpdate);
-				return new Integer (1);
-			}catch (Exception e) {
-				return new Integer (0);
-			}
-		}
-		
-		///////////////////////////Methode supprimer un Proprietaire ////////////////////////
-		
-		/**
-		 * 
-		 * Methode supprimer un Proprietaire
-		 * 
-		 * @param id_prop
-		 * @return
-		 */
-		@RequestMapping(value="/delete/{id_param}", method=RequestMethod.DELETE, produces="application/json")
-		public int deletePropWS(@PathVariable("id_param") int id_prop){
-			try{
-				proprietaireService.deleteService(id_prop);
-				return new Integer (1);	
-			}catch (Exception e) {
-				return new Integer (0);
-			}
-		}
-		
-		///////////////////////////Methode afficher la liste de tout les proprietaires ////////////////////////
 
-		/**
-		 * Methode afficher la liste de tout les proprietaires
-		 * 
-		 * @return
-		 */
-		@RequestMapping(value="/getAll",method=RequestMethod.GET, produces="application/json")
-		public List<Proprietaire> getAllPropWS(){
-			return proprietaireService.getAllProprietaire();
+	/////////////////////////// Methode modifier un Proprietaire
+	/////////////////////////// ////////////////////////
+
+	/**
+	 * Methode modifier un Proprietaire
+	 * 
+	 * @param propUpdate
+	 * @return boolean succes/echec
+	 */
+	@RequestMapping(value = "/update", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+	public int updatePropWS(@RequestBody Proprietaire propUpdate) {
+		try {
+			// Update ou actualisation d'un propriétaire en Bdd
+			proprietaireService.updateService(propUpdate);
+			// Renvoie 1 dans la console si l'opération est réalisée avec succès
+			return new Integer(1);
+		} catch (Exception e) {
+			// Renvoie 1 dans la console si l'opération n'a pas été réalisée
+			// avec succès
+			return new Integer(0);
 		}
-		
-		///////////////////////////Methode afficher proprietaire par id ////////////////////////
-		
-		/**
-		 * Methode afficher proprietaire par id 
-		 * 
-		 * @param id_prop
-		 * @return
-		 */
-		@RequestMapping(value="/getById",method=RequestMethod.GET, produces="application/json")
-		public Proprietaire getPropById(@PathVariable("id_param") int id_prop){
-				return proprietaireService.getProprietaireById(id_prop); 
-	
-		}
-		
 	}
 
+	/////////////////////////// Methode supprimer un Proprietaire
+	/////////////////////////// ////////////////////////
 
-	
+	/**
+	 * 
+	 * Methode supprimer un Proprietaire
+	 * 
+	 * @param id_prop
+	 * @return boolean succes/echec
+	 */
+	@RequestMapping(value = "/delete/{id_param}", method = RequestMethod.DELETE, produces = "application/json")
+	public int deletePropWS(@PathVariable("id_param") int id_prop) {
+		try {
+			// Suppression d'un propriétaire en Bdd
+			proprietaireService.deleteService(id_prop);
+			// Renvoie 1 dans la console si l'opération est réalisée avec succès
+			return new Integer(1);
+		} catch (Exception e) {
+			// Renvoie 1 dans la console si l'opération n'a pas été réalisée
+			// avec succès
+			return new Integer(0);
+		}
+	}
 
-	
-	
+	/////////////////////////// Methode afficher la liste de tous les
+	/////////////////////////// proprietaires ////////////////////////
 
+	/**
+	 * Methode afficher la liste de tout les proprietaires
+	 * 
+	 * @return liste des propriétaires en bdd
+	 */
+	@RequestMapping(value = "/getAll", method = RequestMethod.GET, produces = "application/json")
+	public List<Proprietaire> getAllPropWS() {
+		// Recherche de la liste des propriétaires en BDD
+		return proprietaireService.getAllProprietaire();
+	}
 
+	/////////////////////////// Methode afficher proprietaire par id
+	/////////////////////////// ////////////////////////
+
+	/**
+	 * Methode afficher proprietaire par id
+	 * 
+	 * @param id_prop
+	 * @return d'un propriétaire by Id
+	 */
+	@RequestMapping(value = "/getById", method = RequestMethod.GET, produces = "application/json")
+	public Proprietaire getPropById(@PathVariable("id_param") int id_prop) {
+		// Rechercher par Id en BDD
+		return proprietaireService.getProprietaireById(id_prop);
+
+	}
+
+}
